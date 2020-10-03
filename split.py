b63d66fae493c6143e5f0288d9f61001a28a9ddd
Sharif Uddin Ruman <30799955+SharifRuman@users.noreply.github.com>
2020-10-03 19:29:19 +0600

    l

--- split.py
+++ split.py
@@ -3,10 +3,10 @@ import numpy as np
 import csv
 from sklearn.model_selection import train_test_split
 
-data=pd.read_csv('HAR1.csv')
+data=pd.read_csv('HAR2.csv')
 data.head()
 a=data
 a_train, a_test = train_test_split(a, test_size=0.30, random_state=0)
 
-a_train.to_csv('HAR1_TRAIN.csv',index=False)
-a_test.to_csv('HAR1_TEST.csv',index=False)
+a_train.to_csv('HAR2_TRAIN.csv',index=False)
+a_test.to_csv('HAR2_TEST.csv',index=False)
